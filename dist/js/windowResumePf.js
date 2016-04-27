24417867212bb65d052c259bc81da6d486457bd8
Jordi Cutillas <jordic@PORTIC.LOCAL>
2016-04-27 17:36:08 +0200

    add news styles

--- dist/js/windowResumePf.js
+++ dist/js/windowResumePf.js
@@ -1,4 +1,76 @@
 $(document).ready(function(){
+  var windowIsOpen = false;
+
+  var selectAmbient = function(elem) {
+    var ambiente = elem.val();
+
+    $('#WindowMessage-text').text('Has elegido el ambiente:');
+
+    if($('#WindowMessage-ambient').length <= 0) {
+      $('<li>', {
+        text: ambiente,
+        id: 'WindowMessage-ambient'
+      }).prependTo($('#WindowMessage-list'));
+    } else {
+        $('#WindowMessage-ambient').text(ambiente);
+    } 
+  };
+
+  var toggleWindow = function() {
+    $('#WindowMessage-button').show();
+    
+    if(windowIsOpen){
+
+      $('#WindowMessage').removeClass('is_open');
+
+    } else {
+        $('#WindowMessage').addClass('is_open');
+    }
+   
+  }
+ 
+
+  $('#masaje').on('click', function() {
+    selectAmbient($(this));
+    
+     
+  });
+
+   $('#copa').on('click', function() {
+    selectAmbient($(this));
+   
+     
+  });
+
+  $('#correr').on('click', function() {
+    selectAmbient($(this));
+   
+     
+  });
+
+  $('#biblio').on('click', function() {
+    selectAmbient($(this));
+    
+     
+  });
+
+    $('#arregla').on('click', function() {
+    selectAmbient($(this));
+   
+     
+  });
+
+      $('#ley').on('click', function() {
+    selectAmbient($(this));
+    
+     
+  });
+
+});
+
+
+
+/*$(document).ready(function(){
 
   $("#masaje").click(function () {        
    var masaje = $('input:radio[name=selec]:checked').val();    
@@ -83,4 +155,4 @@ $('#close_x').click(function (e) {
     $('#selection').fadeIn(80).animate({ "right": "+=250px" }, "fast" );
     $('#close').fadeIn(80).animate({ "right": "+=250px" }, "fast" );
   }           
-});
+});*/
