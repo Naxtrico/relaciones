c72cfb4b88a2dac078801e6f6b30e230bdf049fb
Jordi Cutillas <jordic@PORTIC.LOCAL>
2016-04-28 17:47:58 +0200

    refactory styles

---
+++ src/js/validateForm.js
@@ -0,0 +1,24 @@
+$('#btn_prueba').click(function(ev){
+              ev.preventDefault();
+
+              var nombre = $('input[name="nombre_usuario"]').val();
+              if(nombre.length < 1 || nombre.length >15){
+                $('input[name="nombre_usuario"]').prev().show();
+                $('input[name="nombre_usuario"]').addClass('error');
+              }else{
+                $('input[name="nombre_usuario"]').prev().hide();
+                $('input[name="nombre_usuario"]').removeClass('error');
+              }
+
+              var email_usuario = $('input[name="email_usuario"]').val();
+              if(email_usuario.length < 0 ){
+                $('input[name="email_usuario"]').prev().show();
+                $('input[name="email_usuario"]').addClass('error');
+              }
+
+              var mobil = $('input[name="mobil"]').val();
+              if(nombre.length < 9){
+                $('input[name="mobil"]').prev().show();
+                $('input[name="mobil"]').addClass('error');
+              }
+          });
